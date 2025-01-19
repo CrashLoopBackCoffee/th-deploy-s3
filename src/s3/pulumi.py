6313b8930a68b11fdc8950646d0671070cdb598a
Tobias Henkel <tobias_henkel@gmx.de>
2025-01-19 11:30:26 +0100

    Add pulumi bucket
    This needs to be used for some stacks due to the free limit of the
    pulumi backend.

---
+++ src/s3/pulumi.py
@@ -0,0 +1,61 @@
+import pulumi as p
+import pulumi_minio
+import pulumi_onepassword as onepassword
+
+
+def create_pulumi_bucket(
+    minio_provider: pulumi_minio.Provider,
+):
+    minio_opts = p.ResourceOptions(provider=minio_provider)
+    bucket = pulumi_minio.S3Bucket(
+        'pulumi',
+        bucket='pulumi',
+        opts=minio_opts,
+    )
+
+    policy_data = {
+        'Version': '2012-10-17',
+        'Statement': [
+            {
+                'Action': ['s3:ListBucket'],
+                'Effect': 'Allow',
+                'Resource': [
+                    bucket.arn,
+                ],
+            },
+            {
+                'Action': ['s3:*'],
+                'Effect': 'Allow',
+                'Resource': [
+                    p.Output.format('{}/*', bucket.arn),
+                ],
+            },
+        ],
+    }
+
+    policy = pulumi_minio.IamPolicy(
+        'pulumi',
+        policy=p.Output.json_dumps(policy_data),
+        opts=minio_opts,
+    )
+
+    bucket_user = pulumi_minio.IamUser(
+        'pulumi',
+        name='pulumi',
+        opts=minio_opts,
+    )
+
+    pulumi_minio.IamUserPolicyAttachment(
+        'pulumi',
+        user_name=bucket_user.name,
+        policy_name=policy.name,
+        opts=minio_opts,
+    )
+
+    onepassword.Item(
+        's3-pulumi',
+        title='Pulumi S3 Token',
+        vault='Pulumi',
+        username=bucket_user.name,
+        password=bucket_user.secret,
+    )
